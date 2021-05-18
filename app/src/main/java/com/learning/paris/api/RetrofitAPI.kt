63124719b346639cf46e98127b33aa52a89fc419
Piscenco M <32290501+piscenco@users.noreply.github.com>
2021-05-18 12:48:24 +0300

    changes to api & layout for messages

--- app/src/main/java/com/learning/paris/api/RetrofitAPI.kt
+++ app/src/main/java/com/learning/paris/api/RetrofitAPI.kt
@@ -10,6 +10,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.POST
 import kotlin.coroutines.suspendCoroutine
 
 object RetrofitAPI : BackendAPI {
@@ -25,6 +26,15 @@ object RetrofitAPI : BackendAPI {
     interface Api {
         @GET("bots")
         fun getBots(): Call<List<Bot>>
+
+        @GET("dialog")
+        fun getDialog(botId: Int): Call<List<Message>>
+
+        @GET("responses")
+        fun getResponseOptions(botId: Int): Call<ResponseOption>
+
+        @POST("respond")
+        fun respond(botId: Int, optionId: Int): Call<Message>
     }
 
     override suspend fun getBots(): List<Bot> {
@@ -45,18 +55,65 @@ object RetrofitAPI : BackendAPI {
             api.getBots().enqueue(callback)
         }
 
-        return bots.filter { it.name.startsWith("B") }
+        return bots //.filter { it.name.startsWith("B") }
     }
 
     override suspend fun getDialog(botId: Int): List<Message> {
-        TODO("Not yet implemented")
+        val messages = suspendCoroutine<List<Message>> { continuation ->
+            val callback = object : Callback<List<Message>> {
+                override fun onResponse(call: Call<List<Message>>, response: Response<List<Message>>) {
+                    if (response.isSuccessful) {
+                        continuation.resumeWith(Result.success(response.body()!!))
+                    } else {
+                        continuation.resumeWith(Result.failure(Exception("Internal error")))
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Message>>, t: Throwable) {
+                    continuation.resumeWith(Result.failure(t))
+                }
+            }
+            api.getDialog(botId).enqueue(callback)
+        }
+        return messages
     }
 
     override suspend fun getResponseOptions(botId: Int): List<ResponseOption> {
-        TODO("Not yet implemented")
+        val responses = suspendCoroutine<List<ResponseOption>> { continuation ->
+            val callback = object : Callback<List<ResponseOption>> {
+                override fun onResponse(call: Call<List<ResponseOption>>, response: Response<List<ResponseOption>>) {
+                    if (response.isSuccessful) {
+                        continuation.resumeWith(Result.success(response.body()!!))
+                    } else {
+                        continuation.resumeWith(Result.failure(Exception("Internal error")))
+                    }
+                }
+
+                override fun onFailure(call: Call<List<ResponseOption>>, t: Throwable) {
+                    continuation.resumeWith(Result.failure(t))
+                }
+            }
+            api.getResponseOptions(botId).enqueue(callback)// Type mismatch. Required: Callback<ResponseOption!>!
+        }
+        return responses
     }
 
     override suspend fun respond(botId: Int, optionId: Int): List<Message> {
-        TODO("Not yet implemented")
+        val responses = suspendCoroutine<List<Message>> { continuation ->
+            val callback = object : Callback<List<Message>> {
+                override fun onResponse(call: Call<List<Message>>, response: Response<List<Message>>) {
+                    if (response.isSuccessful) {
+                        continuation.resumeWith(Result.success(response.body()!!))
+                    } else {
+                        continuation.resumeWith(Result.failure(Exception("Internal error")))
+                    }
+                }
+                override fun onFailure(call: Call<List<Message>>, t: Throwable) {
+                    continuation.resumeWith(Result.failure(t))
+                }
+            }
+            api.respond(botId, optionId).enqueue(callback) //Required: Callback<Message!>!
+        }
+        return responses
     }
 }
